409c7159863717ae4cdb11ecbca9917e39fe8695
Sieg <siegblink@gmail.com>
2019-09-14 22:21:45 +0800

    Finish basic form features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
+  const [text, setText] = React.useState('')
+  const [checkedState, setCheckedState] = React.useState(true)
+  const [outputText, setOutputText] = React.useState('')
+
+  function handleTextChange(event) {
+    const { value } = event.target
+    setText(value)
+  }
+
+  function handleCheckStateChange(event) {
+    const { checked } = event.target
+    setCheckedState(checked)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const lowercaseText = checkedState
+      ? text
+          .toLocaleLowerCase()
+          .replace(/\./gi, '')
+          .replace(/\s/gi, '')
+      : text
+    setText('')
+    setOutputText(lowercaseText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='checkbox'>
+          <input
+            className='checkbox__input'
+            type='checkbox'
+            defaultChecked={checkedState}
+            onChange={handleCheckStateChange}
+          />
+          <label className='checkbox__label'>clean</label>
+        </div>
+        <div className='form__input-field'>
+          <input
+            className='form__text-field'
+            type='text'
+            value={text}
+            onChange={handleTextChange}
+          />
+          <label className='form__label'>Enter text</label>
+        </div>
+      </form>
+
+      <div className='output'>
+        <div className='output__header-text'>Output</div>
+        {outputText === '' ? (
+          <div className='output__main-content'>No data to display.</div>
+        ) : (
+          <div className='output__main-content'>{outputText}</div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
